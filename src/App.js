c06e40fe4e940e3725f6de365ef5bff464cd8ea1
ManminderTomar1223 <manminder@limitinfinity.org>
2020-06-07 18:11:42 +0530

    Create project from create-react-app and change code for react-router-dom libreary example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const HomePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+
 }
 
-export default App;
+const TopicPage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Topic Page</h1>
+        </div>
+    )
+
+}
+
+const TopicDetailsPage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Topic Details Page</h1>
+        </div>
+    )
+
+}
+
+const App = () => (
+    <div>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/topic' component={TopicPage}></Route>
+        <Route path='/topic/:topicID' component={TopicDetailsPage}></Route>
+    </div>
+)
+
+export default App   
